6dbd4e70a576f2868a3de86e6ab1dcc75e217e12
T. Buktu <tbuktu@hotmail.com>
2012-03-19 19:32:17 -0700

    Fix incompatible pointer warnings

--- src/hash.c
+++ src/hash.c
@@ -1,14 +1,14 @@
 #include <openssl/sha.h>
 #include "hash.h"
 
-void ntru_sha256(unsigned char *input, int input_len, unsigned char *digest) {
+void ntru_sha256(char *input, int input_len, char *digest) {
     SHA256_CTX context;
     SHA256_Init(&context);
     SHA256_Update(&context, input, input_len);
     SHA256_Final((unsigned char*)digest, &context);
 }
 
-void ntru_sha512(unsigned char *input, int input_len, unsigned char *digest) {
+void ntru_sha512(char *input, int input_len, char *digest) {
     SHA512_CTX context;
     SHA512_Init(&context);
     SHA512_Update(&context, input, input_len);
--- src/hash.h
+++ src/hash.h
@@ -1,8 +1,8 @@
 #ifndef HASH_H
 #define HASH_H
 
-void ntru_sha256(unsigned char *input, int input_len, unsigned char *digest);
+void ntru_sha256(char *input, int input_len, char *digest);
 
-void ntru_sha512(unsigned char *input, int input_len, unsigned char *digest);
+void ntru_sha512(char *input, int input_len, char *digest);
 
 #endif
