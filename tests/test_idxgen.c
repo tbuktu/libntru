132430acf99ee391b05448afbbd98238f014f0b7
Tim Buktu <tbuktu@hotmail.com>
2014-03-01 10:28:41 +0100

    Add another test case

--- tests/test_idxgen.c
+++ tests/test_idxgen.c
@@ -8,7 +8,7 @@
 /** number of calls to IGF */
 #define NUM_ITER 1000
 
-/* tests the average of multiple indices */
+/* tests the IGF-2 implementation */
 int test_idxgen() {
     /* seed random number generator */
     time_t rs;
@@ -24,6 +24,8 @@ int test_idxgen() {
     struct NtruEncParams params = APR2011_743_FAST;
     double avg = 0;
     NtruIGFState s;
+
+    /* sanity check for the avg value */
     ntru_IGF_init(seed, sizeof seed, &params, &s);
     int idx;
     for (i=0; i<NUM_ITER; i++) {
@@ -33,6 +35,13 @@ int test_idxgen() {
     avg /= NUM_ITER;
 
     int valid = fabs((params.N/2.0)-avg) < 30;
+
+    /* test reproducability */
+    ntru_IGF_init(seed, sizeof seed, &params, &s);
+    int last_idx = idx;
+    for (i=0; i<NUM_ITER; i++)
+        ntru_IGF_next(&s, &idx);
+    valid &= idx == last_idx;
 
     print_result("test_idxgen", valid);
     return valid;
