966c348f2e033ac5be210b2681486e26ddefa356
Tim Buktu <tbuktu@hotmail.com>
2014-03-05 19:48:42 +0100

    Add another test case

--- tests/test_idxgen.c
+++ tests/test_idxgen.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 #include "encparams.h"
 #include "idxgen.h"
 #include "test_util.h"
 
 /** number of calls to IGF */
 #define NUM_ITER 1000
+#define NUM_ITER_LARGE 100000
 
 /* tests the IGF-2 implementation */
 int test_idxgen() {
@@ -42,6 +44,17 @@ int test_idxgen() {
     for (i=0; i<NUM_ITER; i++)
         ntru_IGF_next(&s, &idx);
     valid &= idx == last_idx;
+
+    /* check that all values between 0 and N-1 are generated */
+    ntru_IGF_init(seed, sizeof seed, &params, &s);
+    int check_list[params.N];
+    memset(check_list, 0, params.N * sizeof check_list[0]);
+    for (i=0; i<NUM_ITER_LARGE; i++) {
+        ntru_IGF_next(&s, &idx);
+        check_list[idx] = 1;
+    }
+    for (i=0; i<params.N; i++)
+        valid &= check_list[i];
 
     print_result("test_idxgen", valid);
     return valid;
