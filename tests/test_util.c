ab57160597747642ae145f9621714bf50f37d4ee
Tim Buktu <tbuktu@hotmail.com>
2014-11-26 20:51:34 +0100

    Fix rand_int() producing non-random output

--- tests/test_util.c
+++ tests/test_util.c
@@ -78,7 +78,7 @@ uint8_t equals_arr(uint8_t *arr1, uint8_t *arr2, uint16_t len) {
 
 uint8_t rand_int(uint16_t N, uint16_t pow2q, NtruIntPoly *poly, NtruRandContext *rand_ctx) {
     uint16_t rand_data[N];
-    if (!rand_ctx->rand_gen->generate((uint8_t*)rand_data, N/2, rand_ctx))
+    if (!rand_ctx->rand_gen->generate((uint8_t*)rand_data, N*2, rand_ctx))
         return 0;
 
     poly->N = N;
