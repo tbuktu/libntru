efb1962145dc41bc28fcf34237213569dbd0f4cb
Tim Buktu <tbuktu@hotmail.com>
2016-02-25 19:44:38 +0100

    Fix buffer length (https://github.com/tbuktu/libntru/issues/27)

--- src/poly.c
+++ src/poly.c
@@ -51,7 +51,7 @@ uint8_t ntru_rand_tern(uint16_t N, uint16_t num_ones, uint16_t num_neg_ones, Ntr
     memset(&coeffs, 0, N * sizeof coeffs[0]);
 
     uint16_t rand_len = num_ones + num_neg_ones + 10;   /* 10 more to avoid calling the RNG again, for up to 10 collisions */
-    uint16_t rand_data[rand_len*2];
+    uint16_t rand_data[rand_len];
     if (ntru_rand_generate((uint8_t*)rand_data, rand_len*2, rand_ctx) != NTRU_SUCCESS)
         return 0;
     uint16_t r_idx = 0;   /* index into rand_data */
